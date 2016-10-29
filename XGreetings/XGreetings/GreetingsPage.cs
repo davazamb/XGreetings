6cc9a9b69cb809f7ca754f74ca23f6937b6ddced
David Zambrano <davazamb10@outlook.com>
2016-10-29 14:45:10 -0400

    Proyecto Actualizado Xamarin.Forms. Creacion de un pageforms

---
+++ XGreetings/XGreetings/GreetingsPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XGreetings
+{
+    public class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            var MyLabel = new Label();
+            MyLabel.Text = "Greetings, Xamarin Forms!";
+            this.Content = MyLabel;
+        }
+    }
+}
