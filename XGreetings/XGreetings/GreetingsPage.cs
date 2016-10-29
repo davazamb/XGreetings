4f98f9f07bb98cc90ca4fdf5e57b8fbfd6c0981f
David Zambrano <davazamb10@outlook.com>
2016-10-29 15:07:02 -0400

    Padding en proyecto

--- XGreetings/XGreetings/GreetingsPage.cs
+++ XGreetings/XGreetings/GreetingsPage.cs
@@ -15,6 +15,8 @@ namespace XGreetings
             var MyLabel = new Label();
             MyLabel.Text = "Greetings, Xamarin Forms!";
             this.Content = MyLabel;
+
+            Padding = new Thickness(0, 20, 0, 0);
         }
     }
 }
